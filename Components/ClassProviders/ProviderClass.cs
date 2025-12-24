c322f3e48689381b722793a523a7f514fbf77ebb
ALEX CESAR TAQUILA CAMASCA <atc757@inlumine.ual.es>
2025-12-24 03:22:41 +0100

    login LATAM + estilos

---
+++ Components/ClassProviders/ProviderClass.cs
@@ -0,0 +1,20 @@
+﻿namespace ArmarioLATAM.Components.ClassProviders
+{
+    using System.Linq;
+    using Microsoft.AspNetCore.Components.Forms;
+    public class LatamFieldClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            var hasErrors = editContext.GetValidationMessages(fieldIdentifier).Any();
+
+            // Solo marcar como válido/ inválido si el campo fue tocado
+            if (editContext.IsModified(fieldIdentifier))
+            {
+         
+            }
+
+            return "";
+        }
+    }
+}
---
+++ Components/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArmarioLATAM.Components.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "Ingresa un correo válido")]
+        public string? Correo { get; set; } 
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [StringLength(50, MinimumLength = 8,
+            ErrorMessage = "La contraseña debe tener entre 8 y 50 caracteres")]
+        public string? Contrasena { get; set; } 
+
+        public bool Recordarme { get; set; }
+    }
+}
