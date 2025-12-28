9b9f74c9ee34ab6d61b772bc8fdb8a036e92bdf3
ALEX CESAR TAQUILA CAMASCA <atc757@inlumine.ual.es>
2025-12-28 23:58:46 +0000

    Tus cambios locales

--- Components/Models/Model.cs
+++ Components/Models/Model.cs
@@ -15,10 +15,37 @@ namespace ArmarioLATAM.Components.Models
 
         public bool Recordarme { get; set; }
     }
+
     public class LoginModelOnlyPassword
     {
         [Required(ErrorMessage = "El correo es obligatorio")]
         [EmailAddress(ErrorMessage = "Ingresa un correo válido")]
         public string Correo { get; set; } = string.Empty;
     }
+
+
+
+    public class EmergencyModel
+    {
+        [Required(ErrorMessage = "Es obligatorio elegir un tipo de emergencia.")]
+        public string EmergencyType { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Es importante que describas él mótivo.")]
+        public string DetalleMotivo { get; set; } = string.Empty;
+    }
+
+    public class AddressModel
+    {
+        [Required(ErrorMessage = "La dirección es obligatoria")]
+         public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Debes seleccionar una provincia.")]
+        public string Province { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Debes seleccionar un distrito.")]
+        public string District { get; set; } = string.Empty;
+
+        public string Reference { get; set; } = string.Empty;
+    }
+
 }
